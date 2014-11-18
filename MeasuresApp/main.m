17e0bc35b72a604f4f6c526b71ba5f3c106a742a
Admin <venko_132@ukr.net>
2014-11-18 20:47:48 +0400

    upload project

---
+++ MeasuresApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MeasuresApp
+//
+//  Created by Admin on 18.11.14.
+//  Copyright (c) 2014 vkCorporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
